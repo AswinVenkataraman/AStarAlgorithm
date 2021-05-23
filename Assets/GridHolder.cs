e8d2e6964f45af1d531c1f5ca40f4c71b0707fe2
AswinVenkataraman <aswinvenkataraman@gmail.com>
2021-05-23 17:55:03 +0530

    Added FIles
    Added Files

---
+++ Assets/GridHolder.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridHolder : MonoBehaviour
+{
+    public int cellNo;
+    public List<GameObject> adjCell;
+
+    GridManager currGridMgr;
+
+    public int currCellRow;
+    public int currCellColumn;
+
+    public bool isStartPosition;
+    public bool isEndPosition;
+    public bool isBlocked;
+    public bool didChecked;
+
+    // gCost For Each Cell is 10, if diagnoal it is 14
+    // hCost is distance formula
+
+    //Cost Related
+    public float gCost;
+    public float hCost;
+    public float tCost;
+
+    public GameObject prevObj;
+    public bool calculatedValues = false;
+
+    public void AssignValues()
+    {
+        adjCell = new List<GameObject>();
+        cellNo = int.Parse(name);
+        currGridMgr = transform.parent.GetComponent<GridManager>();
+
+        currCellRow = cellNo / currGridMgr.columnCount;
+        if (cellNo % currGridMgr.columnCount == 0)
+            currCellRow -= 1;
+
+        currCellColumn = cellNo % currGridMgr.columnCount;
+        if (cellNo % currGridMgr.columnCount == 0)
+            currCellColumn = currGridMgr.columnCount;
+
+        if (isBlocked)
+            GetComponent<Image>().color = Color.black;
+
+        if (isStartPosition)
+            GetComponent<Image>().color = Color.magenta;
+
+        if (isEndPosition)
+            GetComponent<Image>().color = Color.red;
+    }
+
+    public void findNeighbours()
+    {
+        for (int i = -1; i <= 1; i++) // Rows
+        {
+            for (int j = -1; j <= 1; j++) // Column
+            {
+                if (i == 0 && j == 0)
+                    continue;
+
+                int targetCell = cellNo - (i * 5) + j;
+
+
+                if (targetCell > currGridMgr.totalCell || targetCell <=0)
+                    continue;
+
+                GridHolder targetGridHolder = transform.parent.transform.Find(targetCell.ToString()).GetComponent<GridHolder>();
+                int targetCellRow = targetGridHolder.currCellRow;
+                int targetCellColumn = targetGridHolder.currCellColumn;
+
+                if (Mathf.Abs(currCellRow - targetCellRow) >= 2 || Mathf.Abs(currCellColumn - targetCellColumn) >= 2)
+                    continue;
+
+                Transform obj = transform.parent.Find(targetCell.ToString());
+
+                if (obj != null)
+                    adjCell.Add(obj.gameObject);
+
+            }
+        }
+    }
+
+    public void calculateCost()
+    {
+        foreach(GameObject go in adjCell)
+        {
+            GridHolder currGridHolder = go.GetComponent<GridHolder>();
+
+            if (currGridHolder.isBlocked)
+                continue;
+
+            currGridHolder.calculatedValues = true;
+
+            currGridHolder.hCost =  Vector2.Distance(currGridHolder.gameObject.transform.position, currGridMgr.endObj.transform.position);
+
+            if (Mathf.Abs(currGridHolder.currCellRow - currCellRow) == 1 && Mathf.Abs(currGridHolder.currCellColumn - currCellColumn) == 1)
+                currGridHolder.gCost = gCost+ 14;
+            else
+                currGridHolder.gCost = gCost+ 10;
+
+            currGridHolder.tCost = currGridHolder.hCost + currGridHolder.gCost;
+
+        }
+    }
+
+}
---
+++ Assets/GridManager.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridManager : MonoBehaviour
+{
+    public int columnCount;
+    public int rowCount;
+    public int totalCell = 50;
+
+    public GameObject starObj;
+    public GameObject endObj;
+
+    public List<GameObject> visitedList;
+
+
+    private void Awake()
+    {
+        visitedList = new List<GameObject>();
+
+        columnCount = GetComponent<GridLayoutGroup>().constraintCount;
+        rowCount = 50 / columnCount;
+
+        int i = totalCell;
+        foreach (Transform grid in transform)
+        {
+            grid.name = i.ToString();
+            grid.transform.Find("CellNo").GetComponent<Text>().text = i.ToString();
+            i--;
+
+        }
+
+        foreach (Transform grid in transform)
+        {
+            GridHolder gridHolder = grid.GetComponent<GridHolder>();
+            gridHolder.AssignValues();
+        }
+
+
+        foreach (Transform grid in transform)
+        {
+            GridHolder gridHolder = grid.GetComponent<GridHolder>();
+            gridHolder.findNeighbours();
+            if (gridHolder.isStartPosition)
+                starObj = gridHolder.gameObject;
+            else if(gridHolder.isEndPosition)
+                endObj = gridHolder.gameObject;
+        }
+
+        // Calling this in delay, since in unity gridlayout takes few seconds to return values.
+        Invoke("AssignValues", 0.5f);
+
+    }
+
+    private void AssignValues()
+    {
+        starObj.GetComponent<GridHolder>().gCost = 0;
+        starObj.GetComponent<GridHolder>().hCost = Vector2.Distance(starObj.transform.position, endObj.transform.position);
+        starObj.GetComponent<GridHolder>().tCost = starObj.GetComponent<GridHolder>().gCost + starObj.GetComponent<GridHolder>().hCost;
+        starObj.GetComponent<GridHolder>().calculatedValues = true;
+        StartCoroutine(startCalcuatingPath(starObj));
+    }
+
+    public IEnumerator startCalcuatingPath(GameObject currPathObj)
+    {
+        yield return new WaitForSeconds(0.1f);
+
+        GridHolder currGridHolder = currPathObj.GetComponent<GridHolder>();
+
+        currGridHolder.gameObject.GetComponent<Image>().color = Color.blue;
+
+        if (currPathObj == endObj)
+        {
+            Debug.Log("Found the End Point");
+            tracePath(currPathObj);
+            StopAllCoroutines();
+            yield break;//Found the path
+        }
+
+        currGridHolder.calculateCost();
+
+        GameObject obj =  getShortestNode(currGridHolder.tCost,currGridHolder.adjCell);
+        if(obj == null)
+        {
+            currGridHolder.gameObject.GetComponent<Image>().color = Color.blue;
+            yield return new WaitForSeconds(0.1f);
+
+            currGridHolder.gameObject.GetComponent<Image>().color = new Color(0.8773585f, 0.73665f, 0.73665f, 1);
+            StartCoroutine(startCalcuatingPath(currGridHolder.prevObj));
+            yield break;
+        }
+
+        visitedList.Add(currPathObj);
+        obj.GetComponent<GridHolder>().prevObj = currGridHolder.gameObject;
+        StartCoroutine(startCalcuatingPath(obj));
+        yield return 0;
+
+    }
+
+    public GameObject getShortestNode(float currTotalCost, List<GameObject> adjList)
+    {
+        float highestCost = float.MaxValue;
+        GameObject foundObj = null;
+
+        foreach(GameObject go in adjList)
+        {
+
+            GridHolder currGridHolder = go.GetComponent<GridHolder>();
+
+
+            if (visitedList.Contains(go) || currGridHolder.tCost > currTotalCost )
+                continue;
+
+            if (currGridHolder.isBlocked)
+                continue;
+
+            if (currGridHolder.tCost < highestCost)
+            {
+                foundObj = currGridHolder.gameObject;
+                highestCost = currGridHolder.tCost;
+            }
+
+        }
+
+        visitedList.Add(foundObj);
+
+        return foundObj;
+    }
+
+    public void tracePath(GameObject currPathObj)
+    {
+        if (currPathObj == null)
+            return;
+
+        currPathObj.GetComponent<Image>().color = Color.green;
+
+        if (currPathObj == starObj)
+            return;
+
+        GridHolder currGridHolder = currPathObj.GetComponent<GridHolder>();
+        tracePath(currGridHolder.prevObj);
+    }
+}
